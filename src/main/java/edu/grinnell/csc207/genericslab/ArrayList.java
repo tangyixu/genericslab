628cb4277870a4671d4e4a763b690adf583136ad
tangyixu <tangyixu@grinnell.edu>
2025-03-05 12:48:20 -0600

    part3 Done

--- src/main/java/edu/grinnell/csc207/genericslab/ArrayList.java
+++ src/main/java/edu/grinnell/csc207/genericslab/ArrayList.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Array;
  *
  * @param <T>
  */
-public class ArrayList<T> {
+public class ArrayList<T extends Comparable <? super T>> {
 
     private static final int INITIAL_SIZE = 8;
     private T[] data;
@@ -83,7 +83,11 @@ public class ArrayList<T> {
      * @param sep
      */
     public void intersperse(T sep) {
-        ;
+        for(int n = 0; n < this.data.length - 2; n+=2) {
+            T hold = this.data[n + 1];
+            this.data[n + 1] = sep;
+            this.data[n + 2] = hold;
+        }
     }
 
     /**
@@ -92,20 +96,42 @@ public class ArrayList<T> {
      * @return
      */
     public T maximum() {
-        ;
+        T max = this.data[0];
+        for (int n = 1; n < this.data.length; n++) {
+            if(this.data[n].compareTo(max) > 0) {
+                max = this.data[n];
+            }
+        }
+        return max;
     }
 
     /**
-     * returns a string representation of the list in the form: [x1, x2, ...,
+     * Returns a String representation of the list in the form: [x1, x2, ...,
      * xk]. where x1, x2, ..., xk are the elements of the list.
      *
      * @return
      */
     public String toString() {
-
+        String result = "";
+        for (int n = 1; n < this.data.length - 1; n++) {
+            result += "[";
+            result += this.data[n] + ",";
+        }
+        return result + this.data[this.data.length - 1] + "]";
     }
 
-    void insertionSort()
-
-: sorts this list using the insertion sort algorithm. Insertion sort proceeds by looping over the elements of the array, maintaining the following invariant:
+    /**
+     * Sorts this list using the insertion sort algorithm. 
+     */
+    public void insertionSort() {
+        for (int n = 0; n < this.data.length; n++) {
+            for (int m = n; m >= 0; m--) {
+                if (this.data[n].compareTo(this.data[m]) < 0) {
+                    T hold = this.data[n];
+                    this.data[n] = this.data[m];
+                    this.data[m] =  hold;
+                }
+            }
+        }
+    }  
 }
